49136a69da3176638f76d34a9e8c72cd03fe93c9
ThunbergOlle <olle.thunberg03@gmail.com>
2018-10-10 21:16:26 +0200

    Added odometer.js :smile:

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,34 +1,44 @@
-import React, { Component } from 'react'
-import './style/counter.css';
-import { makeRequest } from '../request';
+import React, { Component } from "react";
+import "./style/counter.css";
+import { makeRequest } from "../request";
+import Odometer from "react-odometerjs";
+import config from "./config";
+import Request from "request";
+import "./style/odometer-theme-default.css";
 export class Counter extends Component {
-    constructor() {
-        super()
-        this.amount = 230;
-        this.req();
-    }
+  constructor() {
+    super();
+    this.odometerValue = 0;
+    this.amount = 230;
+    this.req();
+  }
 
-    req() {
-        makeRequest((subs) => {
-            this.amount = subs;
-            this.setState({ amount: subs });
-            console.log(this.amount);
-            return;
-        });
-    }
-    render() {
-        // makeRequest((subs) => {
-        //     this.amount = subs;
-        //     this.setState({amount : subs});
-        //     console.log(this.amount);
-
-        // });
-        return (
-            <div className="center">
-                <h1 className="big">{this.amount}</h1>
-            </div>
-        )
-    }
+  req() {
+    setInterval(subs => {
+      Request(
+        "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC-lHJZR3Gqxm24_Vd_AJ5Yw&key=" +
+          config.api_key,
+        (err, res, body) => {
+          if (err) throw err;
+          body = JSON.parse(body);
+          let subscriberCount = body.items[0].statistics.subscriberCount;
+          this.amount = subscriberCount;
+          this.setState({ amount: subs });
+          console.log(this.amount);
+        }
+      );
+    }, 2500);
+  }
+  render() {
+    return (
+      <div className="center">
+        <h1 className="big">
+          {" "}
+          <Odometer format="d" duration={500} value={this.amount} />
+        </h1>
+      </div>
+    );
+  }
 }
 
-export default Counter
+export default Counter;
--- src/request.js
+++ src/request.js
@@ -2,10 +2,5 @@ const request = require('request');
 const config = require('./components/config');
 module.exports.makeRequest = (callback) => {
     console.log('Testing request module that works');
-    request('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCzfu8Y5CsN7OCLJhZy_fH6w&key=' + config.api_key, (err, res, body) => {
-        if(err) throw err;
-        body = JSON.parse(body);
-        let subscriberCount = body.items[0].statistics.subscriberCount;
-        callback(subscriberCount);
-    });
+
 }
